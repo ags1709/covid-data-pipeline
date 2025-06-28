fcca2058a2089c9e0baef9da43f8a5dec20ce1b9
Anders Greve Soerensen <anders1709@gmail.com>
2025-06-28 10:20:20 +0200

    Change function name for clarification

--- etl/transform.py
+++ etl/transform.py
@@ -1,12 +1,11 @@
 import pandas as pd
 
 
-def clean_covid_data(df, country="Denmark"):
+def transform_covid_data(df, country="Denmark"):
     df_country = df[df["location"] == country].copy()
 
     df_country = df_country[["date", "location", "new_cases", "new_deaths", "total_cases"]].dropna()
 
-    # df_country["date"] = pd.to_datetime(df_country["date"])
     df_country = df_country.sort_values("date")
 
     return df_country
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from etl.extract import fetch_covid_data
-from etl.transform import clean_covid_data
+from etl.transform import transform_covid_data
 from etl.load import save_to_sqlite
 
 def run_pipeline():
     df_raw = fetch_covid_data()
-    df_cleaned = clean_covid_data(df_raw)
-    save_to_sqlite(df_cleaned, "data/covid.db")
+    df_transformed = transform_covid_data(df_raw)
+    save_to_sqlite(df_transformed, "data/covid.db")
 
 
 if __name__ == "__main__":
